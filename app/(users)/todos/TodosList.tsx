43d4b20913e25093b06817653f4e47530679bb40
Roop-TL <roop@talkinglands.com>
2022-11-23 01:50:10 +0530

    Implemented Todo and search functionality with NextJs 13, using SSR and CSR

---
+++ app/(users)/todos/TodosList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import { Todo } from "../../../typings";
+
+const fetchTodos = async () => {
+  //timeout for random 1 to 5 secs
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const todos: Todo[] = await res.json();
+  return todos;
+};
+
+async function TodosList() {
+  const todos = await fetchTodos();
+  return (
+    <>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          <Link href={`/todos/${todo.id}`}> Todo : {todo.id} </Link>
+        </p>
+      ))}
+    </>
+  );
+}
+
+export default TodosList;
